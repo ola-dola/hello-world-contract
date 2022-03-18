d4e032592bff3edb73d1709d7b36f6784cc7afd3
Ola Oredola <oredolaolamide@gmail.com>
2022-03-18 20:05:09 +0100

    rework contract to allow customization of greeting message

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,33 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 
-// Specifies the version of Solidity, using semantic versioning.
-// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
-pragma solidity >=0.7.3;
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
 
 // Defines a contract named `HelloWorld`.
 // A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
 contract HelloWorld {
+    // Declares a state variable `message` of type `string`.
+    // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
+    string public message;
+    string public salutation;
 
-   //Emitted when update function is called
-   //Smart contract events are a way for your contract to communicate that something happened on the blockchain to your app front-end, which can be 'listening' for certain events and take action when they happen.
-   event UpdatedMessages(string oldStr, string newStr);
+    // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
+    // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+    constructor() {
+        // Accepts a string argument `initMessage` and sets the value into the contract's `message` storage variable).
+        salutation = "Hello";
+        message = string(abi.encodePacked(salutation, "World"));
+    }
 
-   // Declares a state variable `message` of type `string`.
-   // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
-   string public message;
+    // A public function that accepts a string argument and updates the `message` storage variable.
+    function updateSalutation(string memory newSalut) public {
+        salutation = newSalut;
 
-   // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
-   // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
-   constructor(string memory initMessage) {
+        console.log("New Salutation is: ", message);
+    }
 
-      // Accepts a string argument `initMessage` and sets the value into the contract's `message` storage variable).
-      message = initMessage;
-   }
+    function greetSpecificUser(string memory name) public view returns (string memory) {
+        console.log(salutation, " ", name);
 
-   // A public function that accepts a string argument and updates the `message` storage variable.
-   function update(string memory newMessage) public {
-      string memory oldMsg = message;
-      message = newMessage;
-      emit UpdatedMessages(oldMsg, newMessage);
-   }
+        return string(abi.encodePacked(salutation, " ", name));
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,10 @@ async function main() {
   const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const hello_world = await HelloWorld.deploy("Hello World!");
+  const hello_world = await HelloWorld.deploy();
   console.log("Contract deployed to address:", hello_world.address);
+
+  await hello_world.updateSalutation("Odiya!!! ");
 }
 
 main()
